f6e2c94b9dec389755fd75639212977e7ab066dc
jihoon <s980817@gmail.com>
2022-11-04 17:19:20 +0900

    today close

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // 텍스트요소
 const $iAm = document.querySelector(".iAm");
-const $me = document.querySelector(".me");
+const $1 = document.querySelector(".text1");
+const $2 = document.querySelector(".text2");
+const $3 = document.querySelector(".text3");
+const $4 = document.querySelector(".text4");
 // 텍스트
 const letters = ["Seok Ji hoon"];
-const letters2 = ["FRONTEND"];
+const letters1 = ["숫자로된 컴퓨터 언어속에서 디자인을,"];
+const letters2 = ["디자인속에 다양한 기능들을,"];
+const letters3 = ["접목시키고 싶은 프론트엔드,"];
+const letters4 = ["웹개발자 석지훈입니다."];
+
 // 글자 입력 속도
 const speed = 100;
 // 현재 지정된 글자
@@ -32,15 +39,64 @@ typing();
   }
 
   const typing = async () => {
+    const letter = letters1[i].split("");
+
+    while (letter.length) {
+      await wait(speed);
+      $1.innerHTML += letter.shift();
+    }
+    await wait(800);
+  };
+  typing(i);
+}
+
+{
+  function wait(ms) {
+    return new Promise((res) => setTimeout(res, ms));
+  }
+
+  const typing = async () => {
+    await wait(2300);
     const letter = letters2[i].split("");
 
     while (letter.length) {
       await wait(speed);
-      $me.innerHTML += letter.shift();
+      $2.innerHTML += letter.shift();
     }
-    await wait(800);
   };
-  typing();
+  typing(i);
+}
+{
+  function wait(ms) {
+    return new Promise((res) => setTimeout(res, ms));
+  }
+
+  const typing = async () => {
+    await wait(4200);
+    const letter = letters3[i].split("");
+
+    while (letter.length) {
+      await wait(speed);
+      $3.innerHTML += letter.shift();
+    }
+  };
+  typing(i);
+}
+{
+  function wait(ms) {
+    return new Promise((res) => setTimeout(res, ms));
+  }
+
+  const typing = async () => {
+    await wait(5800);
+    const letter = letters4[i].split("");
+
+    while (letter.length) {
+      await wait(speed);
+      $4.innerHTML += letter.shift();
+    }
+  };
+  typing(i);
 }
 
 // 넘기기
@@ -111,3 +167,5 @@ window.addEventListener("scroll", () => {
 window.addEventListener("resize", () => {
   cardFlipOnScroll.init();
 });
+
+// 버튼 클릭시 원하는 곳으로 스크롤 이동!
